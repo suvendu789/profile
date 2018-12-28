368bc08a8305ec1c6bf147c44eabc45051279bad
suvendu das <suvendu789@gmail.com>
2018-12-28 20:49:15 +0530

    CSS_Sample Completed

--- src/app/projects/css-proj/sample/sample.component.ts
+++ src/app/projects/css-proj/sample/sample.component.ts
@@ -7,12 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SampleComponent implements OnInit {
 
-  kids = document.getElementById("body").children;
+  public body0 = "body1";
+  public body1 = "body2";
+  public body2 = "body3";
+  public body3 = "body4";
+  public body4 = '';
+  public body: string;
 
-  constructor() { }
+  constructor() { 
+    this.body = this.body0;
+  }
 
   ngOnInit() {
   }
 
-  
+  reStyle(n) {
+    switch (n) {
+      case 0:
+        this.body = this.body0;
+        break;
+      case 1:
+        this.body = this.body1;
+        break;
+      case 2:
+        this.body = this.body2;
+        break;
+      case 3:
+        this.body = this.body3;
+        break;
+    
+      default:
+      this.body = this.body4;
+        break;
+    }
+  }
+
 }
